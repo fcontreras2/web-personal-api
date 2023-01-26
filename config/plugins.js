d7b76633468a1763bbbc7dfcd4502edddcb82860
Freddy Contreras <freddycontreras3@gmail.com>
2023-01-26 12:15:24 -0500

    feat: update  schema for profile

--- config/plugins.js
+++ config/plugins.js
@@ -2,5 +2,10 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 10, // Default is 5
+    }
+  },
   ckeditor: true
 });
