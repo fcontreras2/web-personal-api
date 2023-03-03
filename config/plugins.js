3f48a56de2cdfe2500167b36948f1268152b98b3
Freddy Contreras <freddycontreras3@gmail.com>
2023-03-03 10:29:18 -0500

    add sort into content

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,12 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
-  'strapi-plugin-populate-deep': {
+  "strapi-plugin-populate-deep": {
     config: {
       defaultDepth: 10, // Default is 5
-    }
+    },
+  },
+  "drag-drop-content-types": {
+    enabled: true,
   },
 });
